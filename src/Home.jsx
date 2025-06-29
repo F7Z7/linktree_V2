dd1711d48f84e5ab65bb37b01dd45b9ff5fa5727
farza <farzanrichu610@gmail.com>
2025-06-29 23:53:25 +0530

    initialised file strcututre

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div className="flex flex-col items-center justify-center w-full bg-blue-500 h-screen">
+            Hey
+        </div>
+    );
+}
+
+export default Home;
