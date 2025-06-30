142e08044c617dfbaeeb838e7e6235556a1a882b
farza <farzanrichu610@gmail.com>
2025-07-01 01:44:46 +0530

    basic skeleton set

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import socials from "./assets/data/socials.jsx";
 
 function Home(props) {
     return (
         <div className="flex flex-col items-center justify-center w-full bg-blue-500 h-screen">
-            Hey
+            {socials.map((social, index) => (
+                <div key={social.id} className=" flex flex-row-reverse bg-violet-400 text-white font-bold text-xl">
+                    {social.socialMedia}
+                    {social.icon}
+                </div>
+            ))}
         </div>
     );
 }
---
+++ src/assets/data/socials.jsx
@@ -0,0 +1,42 @@
+import {Instagram,Facebook,Twitter,Music,Github,Linkedin} from "lucide-react";
+
+const socials=[
+    {
+      id: 1,
+      socialMedia:"Instagram",
+      link:"#",
+        icon:<Instagram />
+    },
+    {
+id: 2,
+        socialMedia:"Twitter",
+        link:"#",
+        icon:<Twitter />
+    },
+    {
+id:3,
+        socialMedia:"Spotify",
+        link:"#",
+        icon:<Music/>
+
+    },
+    {
+      id:4,
+      socialMedia:"Linkedin",
+      link:"#",
+      icon:<Linkedin />
+    },
+    {
+        id:5,
+        socialMedia:"Facebook",
+        link:"#",
+        icon:<Facebook />
+    },
+    {
+        id:6,
+        socialMedia:"Github",
+        link:"#",
+        icon:<Github />
+    }
+]
+export default socials;
