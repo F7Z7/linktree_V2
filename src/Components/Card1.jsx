e5109392c54ac01b686ceefb719b7e0e48247a75
Farzan R S <farzanrichu610@gmail.com>
2026-06-13 23:31:06 +0530

    rename Card.jsx to Card1.jsx

--- src/Components/Card1.jsx
+++ src/Components/Card1.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 
-function Card({socials}) {
+function Card1({socials}) {
     return (
         <div className="flex flex-col items-center justify-center w-full gap-3 h-screen">
             {socials.map((social, index) => (
                 <div key={social.id} className={`flex flex-row-reverse items-center gap-2 cursor-pointer w-full justify-center max-w-[200px] p-2 rounded-2xl ${social.hoverColor} transition-colors duration-100 ease-in-out hover:text-black bg-violet-400 text-white font-bold text-xl`}>
-                    {social.socialMedia}
+                    <span className="relative" {social.socialMedia}/>
+                    <span className="absolute right-0 bottom-0 left-0 flex items-center" {social.userId}/>
                     <span className="inline-block">{social.icon}</span>
                 </div>
             ))}
@@ -14,4 +15,4 @@ function Card({socials}) {
     );
 }
 
-export default Card;
+export default Card1;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import socials from "./assets/data/socials.jsx";
-import Card from "./Components/Card.jsx";
+import Card1 from "./Components/Card1.jsx";
 
 function Home() {
     return (
         <div>
-          <Card socials={socials} />
+          <Card1 socials={socials} />
         </div>
     );
 }
