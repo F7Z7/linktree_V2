6eb1842338f9460f6712bf57e7bb8fd3aae2723c
Farzan R S <farzanrichu610@gmail.com>
2026-06-14 15:57:08 +0530

    set the background

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import './App.css'
 import Home from "./Home.jsx";
 import Card1 from "./Components/Card1.jsx";
 import socials from "./assets/data/socials.jsx";
+import MovingGradient from "./Components/movingGrad.js";
+import Background from "./Components/background.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
       <>
+          <Background/>
           <Home/>
       </>
   )
