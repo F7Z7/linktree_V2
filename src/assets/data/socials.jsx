84d5cf1633b645573c01273e3ff2132a2c8a7fb3
Farzan R S <farzanrichu610@gmail.com>
2026-06-14 16:58:34 +0530

    added social links

--- src/assets/data/socials.jsx
+++ src/assets/data/socials.jsx
@@ -1,41 +1,43 @@
 import { Instagram, Facebook, Twitter, Music, Github, Linkedin } from "lucide-react";
 
+const template = "https://";
+
 const socials = [
     {
         id: 1,
         socialMedia: "Instagram",
         icon: <Instagram size={20} />,
-        url: "#"
+        url: `${template}instagram.com/your_username/`
     },
     {
         id: 2,
-        socialMedia: "Twitter",
+        socialMedia: "X",
         icon: <Twitter size={20} />,
-        url: "#"
+        url: `${template}x.com/RichuFarzan`
     },
     {
         id: 3,
         socialMedia: "Spotify",
         icon: <Music size={20} />,
-        url: "#"
+        url: `${template}open.spotify.com/user/31cjgho6k22vjctzwokkf365svxi`
     },
     {
         id: 4,
         socialMedia: "LinkedIn",
         icon: <Linkedin size={20} />,
-        url: "#"
+        url: `${template}linkedin.com/in/farzan7/`
     },
     {
         id: 5,
         socialMedia: "Facebook",
         icon: <Facebook size={20} />,
-        url: "#"
+        url: `${template}facebook.com/farzan.richu/`
     },
     {
         id: 6,
         socialMedia: "GitHub",
         icon: <Github size={20} />,
-        url: "#"
+        url: `${template}github.com/f7z7`
     }
 ];
 
